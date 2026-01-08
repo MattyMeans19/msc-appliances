2ca4f80bcf96636db1d8aa761ee396da8992d64b
Matt <mattymeans@gmail.com>
2026-01-08 00:56:49 -0700

    changed image size on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Appliances from "@/public/Appliances.png";
 
 export default function Home() {
   return (
-    <div className="grow m-15 mt-0 border-10 border-gray-400/60 rounded-4xl grid grid-cols-3 p-10">
+    <div className="grow flex flex-col">
         <Image 
           src={Appliances}
-          width={1024}
-          height={1024}
+          width={800}
+          height={800}
           alt="Array of appliances for display"
         />
     </div>
